1273f84e3679f783a372656a3a1e55aa0bc241da
clearyourcash <byseum@mail.ru>
2021-07-27 09:49:50 +0300

    added validation for form

--- js/main.js
+++ js/main.js
@@ -48,4 +48,25 @@ $(document).ready(function () {
     modalOverlay.removeClass('modal__overlay--visible');
     modalDialog.removeClass('modal__dialog--visible');
   }
+  // Обработка форм
+  $(".form").each(function() {
+    $(this).validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        required: "Please specify your name",
+        minlength: "The name must not be shorter than 2 letters",
+      },
+      email: {
+        required: "E-mail is required",
+        email: "Your email address must be in the format of name@domain.com",
+    },
+      phone: {
+        required: "Phone is required",
+      },
+  },
+  });
+  })
+
+
 });
